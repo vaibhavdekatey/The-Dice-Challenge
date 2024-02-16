ebb7ef693fc519e1b578812a6e95a9c749a71f42
Vaibhav Dekatey <57607581+vaibhavdekatey@users.noreply.github.com>
2024-02-16 21:33:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+
+var randomDice1 = "dice" + randomNumber1 + ".png";
+var randomImgSrc1 = "images/" + randomDice1;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src" , randomImgSrc1);
+
+var randomDice2 = "dice" + randomNumber2 + ".png";
+var randomImgSrc2 = "images/" + randomDice2;
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src" , randomImgSrc2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🔴Player 1 Wins";
+
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins🔴";  
+}
+
+else {
+    document.querySelector("h1").innerHTML = "It's a Draw!⛳";  
+}
